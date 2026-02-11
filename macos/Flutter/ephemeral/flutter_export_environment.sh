dc8a5e3ace70d8eb3a0370c671500f50fd157960
Sergio C. <sercar88@gmail.com>
2026-02-11 18:25:55 +0100

    fix: run updates

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/xrok/fvm/versions/3.32.6"
-export "FLUTTER_APPLICATION_PATH=/Users/xrok/pumpProgress/pump-progress-frontend"
+export "FLUTTER_APPLICATION_PATH=/Users/xrok/repos/pumpProgress/pump-progress-frontend"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=3.7.3"
-export "FLUTTER_BUILD_NUMBER=18"
+export "FLUTTER_BUILD_NAME=3.9.0"
+export "FLUTTER_BUILD_NUMBER=20"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
