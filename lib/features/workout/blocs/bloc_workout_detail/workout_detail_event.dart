3fcfc950d194a91681351073778df150185eafde
Sergio Carbone <sercar88@gmail.com>
2026-05-11 17:56:12 +0200

    feat: add rename and delete events and statuses to WorkoutDetailBloc

--- lib/features/workout/blocs/bloc_workout_detail/workout_detail_event.dart
+++ lib/features/workout/blocs/bloc_workout_detail/workout_detail_event.dart
@@ -26,3 +26,13 @@ class UpdateSearchValueWorkoutDetailEvent extends WorkoutDetailEvent {
   const UpdateSearchValueWorkoutDetailEvent({required this.searchValue});
   final String searchValue;
 }
+
+class RenameWorkoutDetailEvent extends WorkoutDetailEvent {
+  const RenameWorkoutDetailEvent({required this.name});
+  final String name;
+
+  @override
+  List<Object> get props => [name];
+}
+
+class DeleteWorkoutDetailEvent extends WorkoutDetailEvent {}
