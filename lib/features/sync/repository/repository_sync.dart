683256045880c5b12f684ea76bf87be8189470a0
Sergio Carbone <sercar88@gmail.com>
2026-05-29 17:06:19 +0200

    feat: _syncGet/_syncPost return counts; syncTables returns SyncResult

--- lib/features/sync/repository/repository_sync.dart
+++ lib/features/sync/repository/repository_sync.dart
@@ -5,6 +5,7 @@ import 'package:pump_progress_frontend/features/muscle/models/entities/entities.
 import 'package:pump_progress_frontend/features/sets/models/entities/entities.dart';
 import 'package:pump_progress_frontend/features/sync/api/api.dart';
 import 'package:pump_progress_frontend/features/sync/local/local_sync.dart';
+import 'package:pump_progress_frontend/features/sync/models/sync_result.dart';
 import 'package:pump_progress_frontend/features/workout/models/entities/entities.dart';
 import 'package:pump_progress_frontend/utils/helpers/app_logger.dart';
 import 'package:pump_progress_frontend/utils/helpers/sql_database_helper/db_rows.dart';
@@ -15,7 +16,7 @@ class RepositorySync {
 
   RepositorySync();
 
-  Future<void> _syncGet<T extends DBRow>({required String tableName}) async {
+  Future<int> _syncGet<T extends DBRow>({required String tableName}) async {
     final lastSync = await localSync.getLastSync(tableName);
     final remoteChanges = await apiSync.getRowsSync<T>(
       tableName: tableName,
@@ -24,9 +25,10 @@ class RepositorySync {
     AppLogger.debug(
         'Remote changes for $tableName: ${remoteChanges.data.length}');
     await localSync.insertGeneric<T>(remoteChanges.data);
+    return remoteChanges.data.length;
   }
 
-  Future<void> _syncPost<T extends DbRowWrite>(
+  Future<SyncPostResult> _syncPost<T extends DbRowWrite>(
       {required String tableName}) async {
     final lastSync = await localSync.getLastSync(tableName);
     final localChanges = await localSync.getDirtyRows<T>(tableName);
@@ -41,17 +43,40 @@ class RepositorySync {
     AppLogger.debug(
         'Remote changes for $tableName: ${remoteChanges.updates.length}');
     await localSync.markClean<T>(tableName, localChanges);
+    return SyncPostResult(
+      sent: localChanges.length,
+      received: remoteChanges.updates.length,
+    );
   }
 
-  Future<void> syncTables() async {
-    await _syncGet<CategoryRow>(tableName: CategoryRow.tableNameStatic);
-    await _syncGet<MuscleRow>(tableName: MuscleRow.tableNameStatic);
-    await _syncGet<EquipmentRow>(tableName: EquipmentRow.tableNameStatic);
-    await _syncGet<ExerciseRow>(tableName: ExerciseRow.tableNameStatic);
+  Future<SyncResult> syncTables() async {
+    var totalReceived = 0;
+    var totalSent = 0;
 
-    await _syncPost<WorkoutRow>(tableName: WorkoutRow.tableNameStatic);
-    await _syncPost<WorkoutExercisesRow>(
+    totalReceived +=
+        await _syncGet<CategoryRow>(tableName: CategoryRow.tableNameStatic);
+    totalReceived +=
+        await _syncGet<MuscleRow>(tableName: MuscleRow.tableNameStatic);
+    totalReceived +=
+        await _syncGet<EquipmentRow>(tableName: EquipmentRow.tableNameStatic);
+    totalReceived +=
+        await _syncGet<ExerciseRow>(tableName: ExerciseRow.tableNameStatic);
+
+    final workoutPost =
+        await _syncPost<WorkoutRow>(tableName: WorkoutRow.tableNameStatic);
+    totalReceived += workoutPost.received;
+    totalSent += workoutPost.sent;
+
+    final workoutExercisesPost = await _syncPost<WorkoutExercisesRow>(
         tableName: WorkoutExercisesRow.tableNameStatic);
-    await _syncPost<SetsRow>(tableName: SetsRow.tableNameStatic);
+    totalReceived += workoutExercisesPost.received;
+    totalSent += workoutExercisesPost.sent;
+
+    final setsPost =
+        await _syncPost<SetsRow>(tableName: SetsRow.tableNameStatic);
+    totalReceived += setsPost.received;
+    totalSent += setsPost.sent;
+
+    return SyncResult(totalReceived: totalReceived, totalSent: totalSent);
   }
 }
